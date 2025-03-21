4416718c0fd019ce80c391106d7443c5da0ab3fd
Raul Galicia <raul.galiciab@gmail.com>
2025-03-20 19:58:22 -0600

    PlanetDisplay styles

--- src/components/PlanetDisplay.jsx
+++ src/components/PlanetDisplay.jsx
@@ -2,17 +2,21 @@ export const PlanetDisplay = ({ img, title, content, source }) => {
 
 
   return (
-    <>
-      <img
-        src={ img.replace("./", "/") }
-        alt={`${title} display`}
-      />
+    <section className="planet-display">
+      <figure>
+        <img
+          src={ img.replace("./", "/") }
+          alt={`${title} display`}
+        />
+      </figure>
       <h2>{ title.toUpperCase() }</h2>
       <p>{ content }</p>
-      <div>
-        <span>Source:</span>
+
+      <div className="source-container">
+        <span>Source :</span>
         <a href={ source } >Wikipedia</a>
+        <img src="/assets/icon-source.svg" alt="Arrow Icon" />
       </div>
-    </>
+    </section>
   )
 }
