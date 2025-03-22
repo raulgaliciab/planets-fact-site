9e2c899c4b4eeaf0ff87754dc0f2bfde848c817a
Raul Galicia <raul.galiciab@gmail.com>
2025-03-21 20:18:26 -0600

    Conditional border bottom color to active NavLink on PlanetNavbar

--- src/components/PlanetNavbar.jsx
+++ src/components/PlanetNavbar.jsx
@@ -4,17 +4,27 @@ export const PlanetNavbar = () => {
 
   const { planetName } = useParams();
 
+
   return (
     <nav className='planet-navbar'>
-      <NavLink to={`/${planetName}/overview`}>
+      <NavLink
+        to={`/${planetName}/overview`}
+        style={({ isActive }) => isActive ? { borderBottom: `4px solid var(--${planetName.toLowerCase()})` } : {}}
+      >
         OVERVIEW
       </NavLink>
 
-      <NavLink to={`/${planetName}/internal-structure`}>
+      <NavLink
+        to={`/${planetName}/internal-structure`}
+        style={({ isActive }) => isActive ? { borderBottom: `4px solid var(--${planetName.toLowerCase()})` } : {}}
+      >
         STRUCTURE
       </NavLink>
 
-      <NavLink to={`/${planetName}/surface-geology`}>
+      <NavLink
+        to={`/${planetName}/surface-geology`}
+        style={({ isActive }) => isActive ? { borderBottom: `4px solid var(--${planetName.toLowerCase()})` } : {}}
+      >
         SURFACE
       </NavLink>
     </nav>
