cb233e08576d07503aaaa664042c22b60b8469d3
Raul Galicia <raul.galiciab@gmail.com>
2025-03-24 18:43:50 -0600

    Home Page styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,15 @@
 export const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home-page">
+      <h2>Welcome to the Planets App</h2>
+      <p>Click on any planet in the menu to look at its information</p>
+      <figure>
+        <img
+          src="assets/planet-earth.svg"
+          alt="Planet Earth"
+          className="earth"
+        />
+      </figure>
+    </section>
   )
 }
