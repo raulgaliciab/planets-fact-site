478be0b7422e0f7f805b714249e1d4237b149fad
Raul Galicia <raul.galiciab@gmail.com>
2025-03-26 19:33:43 -0600

    Tablet media query; planet-details styles

--- src/components/PlanetDisplay.jsx
+++ src/components/PlanetDisplay.jsx
@@ -10,13 +10,16 @@ export const PlanetDisplay = ({ img, title, content, source }) => {
           className={title.toLowerCase()}
         />
       </figure>
-      <h2>{ title.toUpperCase() }</h2>
-      <p>{ content }</p>
 
-      <div className="source-container">
-        <span>Source :</span>
-        <a href={ source } >Wikipedia</a>
-        <img src="/assets/icon-source.svg" alt="Arrow Icon" />
+      <div className="planet-info">
+        <h2>{ title.toUpperCase() }</h2>
+        <p>{ content }</p>
+
+        <div className="source-container">
+          <span>Source :</span>
+          <a href={ source } >Wikipedia</a>
+          <img src="/assets/icon-source.svg" alt="Arrow Icon" />
+        </div>
       </div>
     </section>
   )
